2f950899a87f7d61e7cb339bec8e26b71b152b17
Philip Märksch <comradephilos@googlemail.com>
2015-06-02 11:29:14 +0200

    added basic GUI

--- main.pas
+++ main.pas
@@ -5,10 +5,22 @@ unit main;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, EditBtn,
+  StdCtrls, Buttons, ComCtrls;
 
 type
+
+  { TForm1 }
+
   TForm1 = class(TForm)
+    BitBtn1: TBitBtn;
+    DirectoryEdit1: TDirectoryEdit;
+    DirectoryEdit2: TDirectoryEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Memo1: TMemo;
+    ProgressBar1: TProgressBar;
   private
     { private declarations }
   public
@@ -17,6 +29,11 @@ type
 
 var
   Form1: TForm1;
+
+const
+  ProgrammVersion = 1.1;
+  ProgrammTitle = 'Transition Tool';
+  ProgrammAuthor = 'Philip Märksch';
 
 implementation
 
