0e97ba7d3ea3e29b4f5b3f1e7f2b4d36eb17b9a2
Philip Märksch <comradephilos@googlemail.com>
2015-06-03 17:19:46 +0200

    ini manages prefixes now

--- main.pas
+++ main.pas
@@ -26,6 +26,7 @@ type
     procedure FormDestroy(Sender: TObject);
   private
     { private declarations }
+    FPrefixes: TStringList;
     procedure LoadIni;
     procedure SaveIni;
   public
@@ -48,6 +49,7 @@ implementation
 
 procedure TMainForm.FormCreate(Sender: TObject);
 begin
+  FPrefixes := TStringList.Create;
   self.Constraints.MinHeight := self.Height;
   self.Constraints.MinWidth := self.Width;
   LoadIni;
@@ -55,7 +57,10 @@ end;
 
 procedure TMainForm.FormDestroy(Sender: TObject);
 begin
+  FPrefixes.Clear;
+  FPrefixes.AddStrings(MemoPrefix.Lines);
   SaveIni;
+  FPrefixes.Free;
 end;
 
 procedure TMainForm.BtnStartClick(Sender: TObject);
@@ -66,11 +71,22 @@ end;
 procedure TMainForm.LoadIni;
 var
   ini: TIniFile;
+  count: Integer; // number of prefixes mentioned in ini-file
+  I: Integer;
 begin
   ini := TIniFile.Create('config.ini');
   try
+    // Paths
     EdtSource.Text := ini.ReadString('Paths', 'Source', '');
     EdtDestination.Text := ini.ReadString('Paths', 'Destination', '');
+    // Prefixes
+    FPrefixes.Clear;
+    count := ini.ReadInteger('Prefixes', 'Count', FPrefixes.Count);
+    for I := 0 to count - 1 do
+    begin
+      FPrefixes.Add(ini.ReadString('Prefixes', IntToStr(I) , ''));
+      MemoPrefix.Lines.Add(FPrefixes[I]);
+    end;
   finally
     ini.Free;
   end;
@@ -79,11 +95,20 @@ end;
 procedure TMainForm.SaveIni;
 var
   ini: TIniFile;
+  I: Integer;
 begin
   ini := TIniFile.Create('config.ini');
   try
+    // Paths
     ini.WriteString('Paths', 'Source', EdtSource.Text);
     ini.WriteString('Paths', 'Destination', EdtDestination.Text);
+    // Prefixes
+    ini.EraseSection('Prefixes');
+    ini.WriteInteger('Prefixes', 'Count', FPrefixes.Count);
+    for I := 0 to FPrefixes.Count - 1 do
+    begin
+      ini.WriteString('Prefixes', IntToStr(I), FPrefixes[I]);
+    end;
   finally
     ini.Free;
   end;
